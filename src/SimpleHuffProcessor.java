4063b3002209180d5b4ac33f0ad78b1746a1a174
Ryan Arifin <ryanarifin134@gmail.com>
2018-04-24 22:44:35 -0500

    standardCountFormat() compression

--- src/SimpleHuffProcessor.java
+++ src/SimpleHuffProcessor.java
@@ -26,7 +26,8 @@ public class SimpleHuffProcessor implements IHuffProcessor {
 
     private IHuffViewer myViewer;
     private HashMap<Integer, String> huffmanMap;// TODO: WHERE SHOULD WE STORE THE HUFFMAN MAP TO USE FOR PREPROCESS AND COMPRESS???
-    
+    private int headerFormat; // TODO: INSTANCE VARIABLE TO KNOW WHICH HEADER FORMAT WE ARE USING FOR COMPRESS?
+    private int[] freqTable; // TODO: IS THIS RIGHT?
     /**
      * Compresses input to output, where the same InputStream has
      * previously been pre-processed via <code>preprocessCompress</code>
@@ -42,11 +43,85 @@ public class SimpleHuffProcessor implements IHuffProcessor {
      * writing to the output file.
      */
     
-    public int compress(InputStream in, OutputStream out, boolean force) throws IOException {
-        throw new IOException("compress is not implemented");
-        //return 0;
+    public int compress(InputStream in, OutputStream out, boolean force) throws IOException { // TODO: ACCOUNT FOR FORCE BOOLEAN???
+        BitInputStream bitInput = new BitInputStream(in);
+        BitOutputStream bitOutput = new BitOutputStream(out);
+        
+        int[] numBitsWritten = new int[1]; // Immutable integer value to keep track of # bits written to compressed file // TODO: SHOULD THIS BE AN INSTANCE VARIABLE? WE ARE PASSING IT AROUND IN METHODS ALOT!
+        
+        writeHeaderInfo(bitInput, bitOutput, numBitsWritten);
+//        for(int i = 0; i < freqTable.length; i++) {
+//            System.out.println("FREQUENCY OF " + i + "  " + freqTable[i] + "  ENCODING: " + huffmanMap.get(i));
+//        }
+        
+        writeCompressedData(bitInput, bitOutput, numBitsWritten);
+        // TODO: DEBUGGING!!!
+        showString("COMPRESS FILE SIZE IN BITS: " + numBitsWritten[0]);
+        // throw new IOException("compress is not implemented");
+        return numBitsWritten[0];
     }
 
+    // Helper method to manage writing all header information to the compressed file
+    // Pre: None
+    // Post: Writes header information (MAGIC_NUMBER, STORE_COUNTS or STORE_TREE constant,
+    //       and information for Huffman Tree to be recreated)
+    private void writeHeaderInfo(BitInputStream bitInput, BitOutputStream bitOutput, 
+            int[] numBitsWritten) {
+        bitOutput.writeBits(BITS_PER_INT, MAGIC_NUMBER);
+        if(headerFormat == STORE_COUNTS) { // Store constant showing file is using Standard Count Format!
+            bitOutput.writeBits(BITS_PER_INT, STORE_COUNTS);
+            writeStandardCountFormatHeader(bitOutput, numBitsWritten);
+        } else { // Store constant showing file is using Standard Tree Format!
+            bitOutput.writeBits(BITS_PER_INT, STORE_TREE);
+            // TODO: GET BACK TO THIS LATER!
+        }  
+        numBitsWritten[0] += BITS_PER_INT * 2; // Includes bits for MAGIC_NUMBER and header format constant
+    }
+    
+    // Helper method to write Standard Count Format header information to compressed file
+    // Pre: None
+    // Post: Writes SCF info. to compressed file to recreate Huffman Tree. For each value in
+    //      alphabet, write frequency of that value in order with BITS_PER_INT bits
+    private void writeStandardCountFormatHeader(BitOutputStream bitOutput, 
+            int[] numBitsWritten) {
+        for(int alphVal = 0; alphVal < ALPH_SIZE; alphVal++) {
+            bitOutput.writeBits(BITS_PER_INT, freqTable[alphVal]); // Write freq. for each alphabet value!
+            numBitsWritten[0] += BITS_PER_INT;
+        }
+    }
+    
+    // Helper method to write compressed data to file reading in input bits from
+    // original file and outputting associated Huffman encoding using the HashMap!
+    // Pre: None
+    // Post: Write original file with new Huffman Encodings
+    private void writeCompressedData(BitInputStream bitInput, BitOutputStream bitOutput, 
+            int[] numBitsWritten) throws IOException{
+        int currentChunkVal = 0; // Gets an integer representation of the current value read
+        while((currentChunkVal = bitInput.readBits(BITS_PER_WORD)) != -1) {
+            String encoding = huffmanMap.get(currentChunkVal);
+            writeHuffEncoding(bitOutput, encoding, numBitsWritten);
+        }
+        
+        String endOfFileCode = huffmanMap.get(PSEUDO_EOF);
+        writeHuffEncoding(bitOutput, endOfFileCode, numBitsWritten);
+    }
+    
+    // Helper method to write Huffman encodings to compressed file
+    // If it's encoding is a 0, write a 0 bit to file
+    // If it's encoding is a 1, write a 1 bit to file
+    // Pre: None
+    // Post: Write this Huffman Encoding to File
+    private void writeHuffEncoding(BitOutputStream bitOutput, String encoding, 
+            int[] numBitsWritten) {
+        for(int indexOfCode = 0; indexOfCode < encoding.length(); indexOfCode++) {
+            if(encoding.charAt(indexOfCode) == '0') {
+                bitOutput.writeBits(1, 0); // Write a 0 bit for each 0 bit in the encoding // TODO:MAKE SURE THIS WORKS!
+            } else {
+                bitOutput.writeBits(1, 1); // Write a 1 bit for each 1 bit in the encoding
+            }
+            numBitsWritten[0]++; // Always adding one bit each time
+        }
+    }
     /**
      * Preprocess data so that compression is possible ---
      * count characters/create tree/store state so that
@@ -64,17 +139,18 @@ public class SimpleHuffProcessor implements IHuffProcessor {
      * reproduce the tree, AND the actual data.
      * @throws IOException if an error occurs while reading from the input file.
      */
-    public int preprocessCompress(InputStream in, int headerFormat) throws IOException { 
+    public int preprocessCompress(InputStream in, int headerFormat) throws IOException { // TODO: ARE THESE TWO DIFFERENT INPUT STREAMS? IF SO, WHEN DO WE NEED TO CLOSE THIS ONE???
         showString("Not working yet");
         myViewer.update("Still not working");
-        int[] freqTable = getFreqTable(in);
-
+        freqTable = getFreqTable(in);
+        
         HuffmanTree encodingTree = new HuffmanTree(freqTable);
+        this.headerFormat = headerFormat;
         
         // Mutable integer value to keep track of total bits in new compressed file (actual data)
         int[] bitsInCompressedData = new int[1]; 
         huffmanMap = getHuffmanMap(encodingTree.getHuffmanRoot(), bitsInCompressedData);
-        System.out.println("HUFFMAN ENCODINGS FOR EASY FILE: " + huffmanMap);
+        
         // Constant to represent the number of bits contained in the MAGIC_NUMBER
         // and the bits in the Store constants (both ints)
         final int CONSTANT_BITS = BITS_PER_INT * 2;
@@ -99,7 +175,7 @@ public class SimpleHuffProcessor implements IHuffProcessor {
     //      alphabet, and the value in number in the array represents that value's frequency
     private int[] getFreqTable(InputStream in) throws IOException{
         int[] frequencyTable = new int[IHuffConstants.ALPH_SIZE];
-        BitInputStream inputReader = new BitInputStream(in);
+        BitInputStream inputReader = new BitInputStream(in); 
         
         int currentChunk = 0; // Gets an integer representation of the current value read
         while((currentChunk = inputReader.readBits(BITS_PER_WORD)) != -1) {
